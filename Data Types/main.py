35a5cc04d367ea2160d7e2cdb3a18a87de7cc4eb
niyati94125 <nanunitya3@gmail.com>
2026-05-21 15:40:23 +0000

    learnt python toady

--- Data Types/main.py
+++ Data Types/main.py
@@ -1,2 +1,29 @@
-n = input("Enter a number")
-print(n);
+#this line below is used for printing
+print("helloNYCstudents")
+#hello how are you
+###i am fine###
+
+"""hello how are you
+i am fine"""
+
+
+#variables
+sher=12
+print(sher)
+chacha=56
+hellono1=45
+
+#anything can be a variable
+
+#no error
+sherNYC = 12
+print(sherNYC)
+ sherNYC = 12
+print(sherNYC)
+sherNYC  = 12
+print(sherNYC)
+
+
+
+
+
